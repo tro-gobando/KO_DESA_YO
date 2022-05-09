2b9319d3741e083bda87276f26023534846792df
gobando <gobando@edenor.com>
2022-05-09 14:23:32 -0300

    re 09-05-22

---
+++ Sprint 27/S1/prod/aws-sdlf-artifacts/code_ddl/Analytics/hrlsotformtc_analyticssec.sql
@@ -0,0 +1,17 @@
+CREATE EXTERNAL TABLE db_koandina_regional_analyticssec.hrlsotformtc_analyticssec(
+TFORM string,
+SPRAS string,
+TXTSHORT string,
+TXTSING string,
+TXTPLUR string
+)
+ROW FORMAT SERDE
+  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+STORED AS INPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
+OUTPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION
+  's3://analyticssec-koandina-prod/regional/sap/hrlsotformtc'
+TBLPROPERTIES (
+  'transient_lastDdlTime'='1611597300')
---
+++ Sprint 27/S1/prod/aws-sdlf-artifacts/code_ddl/Stage/hrlsotformtc_stagesec.sql
@@ -0,0 +1,18 @@
+CREATE EXTERNAL TABLE db_koandina_regional_stage.hrlsotformtc_stagesec(
+TFORM string,
+SPRAS string,
+TXTSHORT string,
+TXTSING string,
+TXTPLUR string
+)
+ROW FORMAT SERDE
+  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+STORED AS INPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
+OUTPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION
+  's3://stagesec-koandina-prod/regional/sap/hrlsotformtc'
+TBLPROPERTIES (
+'compressionType'='SNAPPY',
+'skip.header.line.count'='1')
