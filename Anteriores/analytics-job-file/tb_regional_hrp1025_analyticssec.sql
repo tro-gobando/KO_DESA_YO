da4c9f838c37c9e8d89ac639dab54fa05ac70d24
emc79 <49279565+emc79@users.noreply.github.com>
2022-05-10 14:40:09 -0300

    Add files  analytics Emi

---
+++ Anteriores/analytics-job-file/tb_regional_hrp1025_analyticssec.sql
@@ -0,0 +1,21 @@
+select
+    PLVAR,
+    OTYPE,
+    OBJID,
+    SUBTY,
+    ISTAT,
+    BEGDA,
+    ENDDA,
+    VARYF,
+    SEQNR,
+    INFTY,
+    OTJID,
+    AEDTM,
+    UNAME,
+    REASN,
+    HISTO,
+    ITXNR,
+    NYEARS,
+    NMONTHS,
+    N_TYPE
+from $SOURCE_DATABASE.hrp1025_stagesec
---
+++ Anteriores/analytics-job-file/tb_regional_hrpat002_analyticssec.sql
@@ -0,0 +1,9 @@
+select
+bukrs,
+pernr_boss,
+orgeh_padre,
+orgtx_padre,
+nivel,
+orgeh_hija,
+orgtx_hija
+from $SOURCE_DATABASE.hrpat002_stagesec
