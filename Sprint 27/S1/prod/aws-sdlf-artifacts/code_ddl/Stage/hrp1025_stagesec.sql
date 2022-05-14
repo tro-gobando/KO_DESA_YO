fb37e01129934cee60a9715471222c9ad32874a9
Emiliano Candia <ecandia@vi-datec.com>
2022-05-14 11:40:54 -0300

    Fix CSV to Parquet

--- Sprint 27/S1/prod/aws-sdlf-artifacts/code_ddl/Stage/hrp1025_stagesec.sql
+++ Sprint 27/S1/prod/aws-sdlf-artifacts/code_ddl/Stage/hrp1025_stagesec.sql
@@ -18,15 +18,12 @@ ITXNR string,
 NYEARS string,
 NMONTHS string,
 N_TYPE string)
-ROW FORMAT SERDE 
-  'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
-WITH SERDEPROPERTIES ( 
-  'quoteChar'='\"', 
-  'separatorChar'='\;') 
-STORED AS INPUTFORMAT 
-  'org.apache.hadoop.mapred.TextInputFormat' 
-OUTPUTFORMAT 
-  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+ROW FORMAT SERDE
+  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+STORED AS INPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
+OUTPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
 LOCATION
   's3://stagesec-koandina-prod/regional/sap/hrp1025/'
 TBLPROPERTIES (
